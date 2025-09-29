c8f7a7d8a5d41f1e34477b5aa42e2f1feb59f038
“carlosrobertodevops” <carlos.roberto.devops@gmail.com>
2025-09-29 15:41:52 -0300

    first commit

---
+++ next.config.mjs
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+import createNextIntlPlugin from 'next-intl/plugin';
+const withNextIntl = createNextIntlPlugin();
+
+const nextConfig = {
+  images: {
+    domains: ["img.clerk.com"],
+  },
+
+};
+
+export default withNextIntl(nextConfig);
