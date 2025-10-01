4efec309f84ee5fb9295848f0df37850f4d52f17
Carlos Roberto <carlos.roberto.devops@gmail.com>
2025-10-01 16:33:06 -0300

    Ajustes de pnpm

--- src/components/LocaleSwitcherSelect.tsx
+++ src/components/LocaleSwitcherSelect.tsx
@@ -8,141 +8,67 @@ import { Locale } from "@/src/i18n/config";
 import { setUserLocale } from "@/src/services/locale";
 
 type Props = {
-  defaultValue: string;
-  items: Array<{ value: string; label: string }>;
-  label: string;
+    defaultValue: string;
+    items: Array<{ value: string; label: string }>;
+    label: string;
 };
 
 export default function LocaleSwitcherSelect({
-  defaultValue,
-  items,
-  label,
+    defaultValue,
+    items,
+    label,
 }: Props) {
-  const [isPending, startTransition] = useTransition();
+    const [isPending, startTransition] = useTransition();
 
-  function onChange(value: string) {
-    const locale = value as Locale;
-    startTransition(() => {
-      setUserLocale(locale);
-    });
-  }
+    function onChange(value: string) {
+        const locale = value as Locale;
+        startTransition(() => {
+            setUserLocale(locale);
+        });
+    }
 
-  return (
-    <div className="relative">
-      <Select.Root defaultValue={defaultValue} onValueChange={onChange}>
-        <Select.Trigger
-          aria-label={label}
-          className={clsx(
-            "rounded-full p-2 transition-colors hover:bg-slate-700",
-            isPending && "pointer-events-none opacity-60",
-          )}
-        >
-          <Select.Icon>
-            <LanguageIcon className="h-6 w-6 text-white transition-colors group-hover:text-purple-300" />
-          </Select.Icon>
-        </Select.Trigger>
-        <Select.Portal>
-          <Select.Content
-            align="end"
-            className="min-w-[8rem] overflow-hidden rounded-lg  bg-purple-600 py-1 shadow-md"
-            position="popper"
-          >
-            <Select.Viewport>
-              {items.map((item) => (
-                <Select.Item
-                  key={item.value}
-                  className="flex cursor-default items-center px-3 py-2 text-base data-[highlighted]:bg-purple-500"
-                  value={item.value}
-                >
-                  <div className="mr-2 w-[1rem]">
-                    {item.value === defaultValue && (
-                      <CheckIcon className="h-5 w-5 text-white" />
+    return (
+        <div className="relative">
+            <Select.Root defaultValue={defaultValue} onValueChange={onChange}>
+                <Select.Trigger
+                    aria-label={label}
+                    className={clsx(
+                        " text-white  rounded-full p-2 transition-colors hover:bg-slate-700",
+                        isPending && "pointer-events-none opacity-60",
                     )}
-                  </div>
-                  <span className="text-white">{item.label}</span>
-                </Select.Item>
-              ))}
-            </Select.Viewport>
-            <Select.Arrow className="fill-purple-600  text-purple-600" />
-          </Select.Content>
-        </Select.Portal>
-      </Select.Root>
-    </div>
-  );
+                >
+                    <Select.Icon>
+                        <LanguageIcon className="h-6 w-6 text-white transition-colors group-hover:text-purple-300" />
+                    </Select.Icon>
+                </Select.Trigger>
+                <Select.Portal>
+                    <Select.Content
+                        align="end"
+                        className="min-w-[8rem] overflow-hidden rounded-lg  bg-purple-600 py-1 shadow-md"
+                        position="popper"
+                    >
+                        <Select.Viewport>
+                            {items.map((item) => (
+                                <Select.Item
+                                    key={item.value}
+                                    className="flex cursor-default items-center px-3 py-2 text-base data-[highlighted]:bg-purple-500"
+                                    value={item.value}
+                                >
+                                    <div className="mr-2 w-[1rem]">
+                                        {item.value === defaultValue && (
+                                            <CheckIcon className="h-5 w-5 text-white" />
+                                        )}
+                                    </div>
+                                    <span className="text-white">
+                                        {item.label}
+                                    </span>
+                                </Select.Item>
+                            ))}
+                        </Select.Viewport>
+                        <Select.Arrow className="fill-purple-600  text-purple-600" />
+                    </Select.Content>
+                </Select.Portal>
+            </Select.Root>
+        </div>
+    );
 }
-
-// "use client";
-
-// import { CheckIcon, LanguageIcon } from "@heroicons/react/24/solid";
-// import * as Select from "@radix-ui/react-select";
-// import clsx from "clsx";
-// import { useTransition } from "react";
-// import { Locale } from "@/src/i18n/config";
-// import { setUserLocale } from "@/src/services/locale";
-
-// setUserLocale;
-
-// type Props = {
-//   defaultValue: string;
-//   items: Array<{ value: string; label: string }>;
-//   label: string;
-// };
-
-// export default function LocaleSwitcherSelect({
-//   defaultValue,
-//   items,
-//   label,
-// }: Props) {
-//   const [isPending, startTransition] = useTransition();
-
-//   function onChange(value: string) {
-//     const locale = value as Locale;
-//     startTransition(() => {
-//       setUserLocale(locale);
-//     });
-//   }
-
-//   return (
-//     <div className="relative">
-//       <Select.Root defaultValue={defaultValue} onValueChange={onChange}>
-//         <Select.Trigger
-//           aria-label={label}
-//           className={clsx(
-//             "rounded-full p-2 transition-colors hover:bg-purple-600 group",
-//             "bg-purple-600",
-//             isPending && "pointer-events-none opacity-60",
-//           )}
-//         >
-//           <Select.Icon>
-//             <LanguageIcon className="h-6 w-6 text-white transition-colors group-hover:text-purple-600" />
-//           </Select.Icon>
-//         </Select.Trigger>
-//         <Select.Portal>
-//           <Select.Content
-//             align="end"
-//             className="min-w-[8rem] overflow-hidden rounded-sm bg-purple-600 py-1 shadow-md"
-//             position="popper"
-//           >
-//             <Select.Viewport>
-//               {items.map((item) => (
-//                 <Select.Item
-//                   key={item.value}
-//                   className="flex cursor-default items-center px-2 py-2 text-base data-[highlighted]:bg-purple-600"
-//                   value={item.value}
-//                 >
-//                   <div className="mr-2 w-[1rem]">
-//                     {item.value === defaultValue && (
-//                       <CheckIcon className="h-5 w-5  text-purple-600" />
-//                     )}
-//                   </div>
-//                   <span className="text-slate-100">{item.label}</span>
-//                 </Select.Item>
-//               ))}
-//             </Select.Viewport>
-//             <Select.Arrow className="fill-purple-800" />
-//           </Select.Content>
-//         </Select.Portal>
-//       </Select.Root>
-//     </div>
-//   );
-// }
