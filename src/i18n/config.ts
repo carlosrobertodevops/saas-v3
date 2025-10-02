5d3019cb00ecf6352d72e5d0e48d503e9ad2f86b
Carlos Roberto <carlos.roberto.devops@gmail.com>
2025-10-02 19:11:26 -0300

    Ajustes

--- src/i18n/config.ts
+++ src/i18n/config.ts
@@ -3,12 +3,12 @@ export type Locale = (typeof locales)[number];
 export const locales = ["en", "pt", "es"] as const;
 export const defaultLocale: Locale = "en";
 
-// export const COOKIE_NAME = "NEXT_LOCALE";
+export const COOKIE_NAME = "NEXT_LOCALE";
 
-// const nextConfig = {
-//   images: {
-//     domains: ["img.clerk.com"],
-//   },
-// };
+const nextConfig = {
+  images: {
+    domains: ["img.clerk.com"],
+  },
+};
 
-// export default nextConfig;
+export default nextConfig;
--- src/services/session.ts
+++ src/services/session.ts
@@ -1,29 +1,27 @@
-// // Clerk
-// import { useAuth } from "@clerk/nextjs";
+// Clerk
+import { useAuth } from "@clerk/nextjs";
 
-// // Tradução
-// import { useTranslations } from 'next-intl';
+// Tradução
+import { useTranslations } from "next-intl";
 
-// export async function loginUser(credentials: {
-//   email: string;
-//   password: string;
-// }) {
+export async function loginUser(credentials: {
+  email: string;
+  password: string;
+}) {
+  const l = useTranslations("Common");
+  // Clerk Auth
+  const { userId, isLoaded } = useAuth();
 
-// const l = useTranslations('Common');
-// // Clerk Auth
-// const { userId, isLoaded } = useAuth();
-
-//   if (isLoaded) {
-//     // Optionally render a loading state
-//   }
-//   // In a real app, the credentials would be checked against a
-//   // database and potentially a session token set in a cookie
-//   return new Promise((resolve) => {
-//     setTimeout(() => {
-//       resolve(
-//           // User is loaded, proceed with login logic
-//           // ...
-//       );
-//     }, 1000);
-//   });
-// }
+  if (isLoaded) {
+    // Optionally render a loading state
+  }
+  // In a real app, the credentials would be checked against a
+  // database and potentially a session token set in a cookie
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+      // User is loaded, proceed with login logic
+      // ...
+    }, 1000);
+  });
+}
